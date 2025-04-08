656f3e24e1b181c1c1fb14f2bb1bb33a35848190
Wdrew232 <wdrew232@gmail.com>
2025-04-08 00:56:10 +0000

    added code

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
-from flask_swagger import swagger
-from flask_cors import CORS
+from flask import Flask, request, jsonify, url_for # type: ignore
+from flask_migrate import Migrate # type: ignore
+from flask_swagger import swagger # type: ignore
+from flask_cors import CORS # type: ignore
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Character,Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,67 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_get_user():
+    user = User.query.all()
+    user_list = [userData.serialize() for userData in user]
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(user_list), 200
 
-    return jsonify(response_body), 200
+@app.route('/character', methods=['GET'])
+def handle_get_Character():
+    character = Character.query.all()
+    character_list = [characterData.serialize() for characterData in character]
+
+    return jsonify(character_list), 200
+
+
+@app.route('/planets', methods=['GET'])
+def handle_get_planet():
+    planets = Planet.query.all()
+    planets_list = [planetData.serialize() for planetData in planets]
+
+    return jsonify(planets_list), 200
+
+
+@app.route('/user', methods=['POST'])
+def handle_user_post():
+    data = request.json
+    new_user = User(
+        id = data["id"], 
+        email = data["email"], 
+        password = data["password"], 
+        is_active = data["is_active"] 
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 200
+
+@app.route('/character', methods=['POST'])
+def handle_character_post():
+    data = request.json
+    new_character = Character(
+        id = data["id"],
+        name = data["name"],
+        age = data["age"],
+        height =  data["height"],
+        weight = data["weight"]
+    )
+    db.session.add(new_character)
+    db.session.commit()
+    return jsonify(new_character.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def handle_planet_post():
+    data = request.json
+    new_planet = Planet(
+        id = data["id"],
+        name = data["name"],
+        population = data["population"],
+        size = data["size"]
+    )
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify(new_planet.serialize()), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from flask_sqlalchemy import SQLAlchemy  # type: ignore
+from sqlalchemy import String, Boolean, Integer, Float  # type: ignore
+from sqlalchemy.orm import Mapped, mapped_column  # type: ignore
 
 db = SQLAlchemy()
 
@@ -8,12 +8,42 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
+        }
+
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=False, nullable=False)
+    age: Mapped[str] = mapped_column(String(5),nullable=True)
+    height: Mapped[float] = mapped_column(nullable=True)
+    weight: Mapped[float] = mapped_column(nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "height": self.height,
+            "weight": self.weight
+        }
+
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    population: Mapped[int] = mapped_column(nullable=False)
+    size: Mapped[float] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "size": self.size
         }
